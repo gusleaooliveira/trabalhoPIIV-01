1a6281ec99b6e9f1f0b8bf7e294660be07bf9a50
Gustavo Leão <gustavo.leao.nogueira.2017@gmail.com>
2020-10-07 14:10:37 -0300

    login

--- controller/controllerUsuarios.js
+++ controller/controllerUsuarios.js
@@ -1,4 +1,7 @@
 const Usuarios = require('../model/modelUsuarios');
+const bcrypt = require('bcrypt');
+const salto = bcrypt.genSaltSync(13);
+ 
 
 exports.listar = (req, res, next) => {
     Usuarios.find({}, (error, usuarios) => {
@@ -9,7 +12,16 @@ exports.listar = (req, res, next) => {
     });
 }
 exports.inserir = (req, res, next) => {
-    let newUsuario = new Usuarios(req.body);
+    let usuario = req.body;
+    let senha = req.body.senha;
+    if(req.body.senha){
+        bcrypt.genSalt(salto, function(err, salt) {
+            bcrypt.hash(senha, salt, function(err, hash) {
+                usuario['senha'] = hash;
+            });
+        });
+    }
+    let newUsuario = new Usuarios(usuario);
     newUsuario.save((err, usuario) => {
         if(err){
             res.status(400).send(error);
@@ -20,6 +32,14 @@ exports.inserir = (req, res, next) => {
 exports.atualizar = (req, res, next) => {
     let id = req.params.id;
     let updateUsuario = req.body;
+    let senha = req.body.senha;
+    if(req.body.senha){
+        bcrypt.genSalt(salto, function(err, salt) {
+            bcrypt.hash(senha, salt, function(err, hash) {
+                updateUsuario['senha'] = hash;
+            });
+        });
+    }
     Usuarios.findOneAndUpdate({ _id: id}, updateUsuario, {new: true}, (err, usuario) => {
         if(err){
             res.status(400).send(error);
@@ -46,17 +66,39 @@ exports.buscarPorId = (req, res, next) => {
     })
 }
 exports.procurar = (req, res, next) => {
-    if(req.query && req.query.email && req.query.senha){
-        let paramEmail = req.query.email;
-        let paramSenha = req.query.senha;
+    if(req.query && req.query.tipo){
         Usuarios.find({
-            email: paramEmail,
-            senha: paramSenha
+            tipo: req.query.tipo
         }, (err, usuario) => {
-            if(err){
-                res.status(400).send(error);
-            }
-            res.status(200).send(usuario);
+            if(err)res.status(400).send(err);
+            res.status(200).send(usuario);            
         });
     }
+    if(req.query && req.query.email && req.query.senha){
+        let paramEmail = req.query.email;
+        let paramSenha  = req.query.senha;
+
+        Usuarios.find({
+            email: paramEmail
+        }, (err, usuario) => {
+            if(err)res.status(400).send(err);
+            let senha = usuario[0]['senha'];
+            bcrypt.compare(paramSenha, senha, (err, verifica) =>{
+                console.log(verifica, senha, paramSenha);
+                if(!verifica){res.status(400).send(verifica);}
+                else {
+                    Usuarios.find({
+                        email: paramEmail,
+                        senha: senha
+                    }, (err, usuario) => {
+                        if(err){res.status(400).send(verifica);}
+                        res.status(200).send(usuario);
+                    });
+                }
+            });            
+        });
+     
+    }
+
+    
 }
